fbce8f01058a745ab1d0207975f28f999919016d
MonarchRyuzaki <shivamganguly2004@gmail.com>
2023-10-27 18:59:50 +0530

    Billing Fixed on Small sizes

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,7 +1,7 @@
 import styles from "../style";
 
 const Feature = ({ bg, icon, title, content }) => (
-  <div className={`flex flex-row flex-1 justify-center items-center ${bg} rounded-lg`}>
+  <div className={`flex flex-row flex-1 justify-center items-center ${bg} rounded-lg md:my-0 my-4 py-3`}>
     <div className="px-6">
       <img src={icon} alt="icon" />
     </div>
